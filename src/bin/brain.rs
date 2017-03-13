ba4d8bfe5dfe783ce38a66a98823ad25ad2cdc5f
jchaput <piinkyz05@gmail.com>
2017-03-12 21:41:45 -0400

    updated cli

--- src/bin/brain.rs
+++ src/bin/brain.rs
@@ -1,20 +1,37 @@
+extern crate clap;
 extern crate brain;
 
-use brain::{Compiler, Machine};
+use clap::{App, Arg};
+use brain::{Compiler, Machine, ByteCode};
+
 use std::io::{self, Read};
 use std::fs::File;
-use std::env;
 
 const ERROR: &'static str = "[Error]";
 
 fn main() {
     let inp = io::stdin();
     let out = io::stdout();
-    let args = env::args().collect::<Vec<String>>();
-    let file_name = match args.last() {
-        Some(name) => name,
-        None => return,
+
+    let matches = App::new("brain")
+        .arg(Arg::with_name("file")
+            .takes_value(true)
+            .short("f")
+            .long("file")
+            .required(true)
+        ).arg(Arg::with_name("bytecode")
+            .short("b")
+            .long("bytecode")
+        ).get_matches();
+
+    let file_name = match matches.value_of("file") {
+        Some(v) => v,
+        None => {
+            println!("{} Source file needed.", ERROR);
+            return
+        },
     };
+
     let mut file = match File::open(file_name) {
         Ok(f) => f,
         Err(_) => {
@@ -34,12 +51,17 @@ fn main() {
 
     let mut cmp = Compiler::new(&code);
     let instructions = cmp.compile();
-    let mut machine = Machine::new(instructions, inp, out);
-    match machine.execute() {
-        Ok(_) => {},
-        Err(e) => {
-            println!("{} {}", ERROR, e);
-            return
+    if matches.is_present("bytecode") {
+        let bytecode = ByteCode::generate_bytecode(instructions);
+        println!("{}", bytecode);
+    } else {
+        let mut machine = Machine::new(instructions, inp, out);
+        match machine.execute() {
+            Ok(_) => {},
+            Err(e) => {
+                println!("{} {}", ERROR, e);
+                return
+            }
         }
     }
 }
