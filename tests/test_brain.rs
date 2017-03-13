252c4cc3343f2f738bcefb3d59a4739833d716d8
jchaput <piinkyz05@gmail.com>
2017-03-12 20:46:01 -0400

    updated test

--- tests/test_brain.rs
+++ tests/test_brain.rs
@@ -4,12 +4,15 @@ use brain::prelude::*;
 
 #[test]
 fn test_with_string() {
-    let mut read_buffer = String::new();
+    let read_buffer = String::new();
     let mut write_buffer: Vec<u8> = Vec::new();
     let code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
-    execute(code, read_buffer.as_bytes(), &mut write_buffer);
-
-    println!("{:?}", write_buffer.iter().map(|c| *c as char).collect::<String>());
-    println!("{:?}", read_buffer);
+   match execute(code, read_buffer.as_bytes(), &mut write_buffer) {
+       Ok(_) => {
+            println!("{:?}", write_buffer.iter().map(|c| *c as char).collect::<String>());
+            println!("{:?}", read_buffer);
+       },
+       Err(e) => panic!(e),
+   }
 }
